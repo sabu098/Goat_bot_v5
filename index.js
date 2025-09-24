e4d2378a591532c22eb12cd8023bf97a0822ac00
goat-bot-azad <talukdershoikat@gmail.com>
2025-09-24 16:59:59 +0600

    Update index.js

--- index.js
+++ index.js
@@ -36,13 +36,3 @@ function startProject() {
 }
 
 startProject();
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Bot is running!');
-});
-
-app.listen(3000, () => {
-  console.log('Uptime server running on port 3000');
-});
